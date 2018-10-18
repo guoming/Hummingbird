e2d3e38fd91b8fef8e640a1cfc0ab61ae00d74e4
guoming <geniusming@qq.com>
2018-10-18 11:45:34 +0800

    update Redis HealthCheck

--- Hummingbird.Extensions.HealthChecks.Redis/HealthCheckBuilderRedisExtensions.cs
+++ Hummingbird.Extensions.HealthChecks.Redis/HealthCheckBuilderRedisExtensions.cs
@@ -1,35 +1,34 @@
-﻿using System;
-using ServiceStack.Redis;
+﻿using StackExchange.Redis;
+using System;
 using System.Linq;
+
 namespace Microsoft.Extensions.HealthChecks
 {
     public static class HealthCheckBuilderRedisExtensions
     {
-        public static HealthCheckBuilder AddRedisCheck(this HealthCheckBuilder builder, string name, string host, int port, string password = null)
+        public static HealthCheckBuilder AddRedisCheck(this HealthCheckBuilder builder, string name, string connectionString)
         {
             Guard.ArgumentNotNull(nameof(builder), builder);
 
-            return AddRedisCheck(builder, name, builder.DefaultCacheDuration, host, port, password);
+            return AddRedisCheck(builder, name, builder.DefaultCacheDuration, connectionString);
         }
 
 
-        public static HealthCheckBuilder AddRedisCheck(this HealthCheckBuilder builder, string name, TimeSpan cacheDuration, string host, int port, string password = null)
+
+        public static HealthCheckBuilder AddRedisCheck(this HealthCheckBuilder builder, string name, TimeSpan cacheDuration, string connectionString)
         {
             builder.AddCheck($"RedisCheck({name})", () =>
             {
                 try
                 {
-                    using (var client = new RedisClient(host, port, password))
+                    ConnectionMultiplexer connect = ConnectionMultiplexer.Connect(new ConfigurationOptions() { });
+                    var response = connect.GetStatus();
+
+                    if (response != null && response.Any())
                     {
-                        var response = client.Info;
-
-                        if (response != null && response.Any())
-                        {
-                            return HealthCheckResult.Healthy($"RedisCheck({name}): Healthy");
-                        }
-                        return HealthCheckResult.Unhealthy($"RedisCheck({name}): Unhealthy");
-
+                        return HealthCheckResult.Healthy($"RedisCheck({name}): Healthy");
                     }
+                    return HealthCheckResult.Unhealthy($"RedisCheck({name}): Unhealthy");
                 }
                 catch (Exception ex)
                 {
