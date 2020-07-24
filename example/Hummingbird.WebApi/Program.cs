c6d69e65a91af80be6ae71f2ac723dad22712a5b
guoming <geniusming@qq.com>
2020-07-24 13:36:17 +0800

    feat: metrics采集到prometheus/influxdb

--- example/Hummingbird.WebApi/Program.cs
+++ example/Hummingbird.WebApi/Program.cs
@@ -2,24 +2,23 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using System.IO;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
 namespace Hummingbird.WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
             BuildWebHost(args).Run();
-
-           
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseHealthChecks("/healthcheck")
+                .UseMetrics()
                 .ConfigureAppConfiguration((builderContext, config) =>
                   {
                       config.SetBasePath(Directory.GetCurrentDirectory());
@@ -33,6 +32,6 @@ namespace Hummingbird.WebApi
                    logging.ClearProviders();
 
                })
-             .Build();
+               .Build();
     }
 }
---
+++ src/Hummingbird.Extensions.Metrics/DependencyInjectionExtersion.cs
@@ -0,0 +1,102 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static partial class DependencyInjectionExtersion
+    {
+        public static IServiceCollection AddMetrics(this IServiceCollection services, IConfigurationSection configurationSection)
+        {
+            var metricsBuilder = App.Metrics.AppMetrics.CreateDefaultBuilder()
+                .OutputMetrics.AsPrometheusPlainText()
+                .OutputMetrics.AsPrometheusProtobuf()
+                .Configuration.Configure(options =>
+                {
+                    options.AddEnvTag();
+                    options.AddAppTag();
+                    options.AddServerTag();
+                    options.Enabled = true;
+                })
+                .ToInfluxDb(configurationSection);
+
+            services.AddSingleton<App.Metrics.IMetricsRoot>(metricsBuilder.Build());
+            return services;
+        }
+
+
+        public static IWebHostBuilder UseMetrics(this IWebHostBuilder hostBuilder)
+        {
+            hostBuilder.ConfigureMetrics((hostingContext, metricsBuilder) =>
+            {
+                metricsBuilder.OutputMetrics.AsPrometheusPlainText();
+                metricsBuilder.OutputMetrics.AsPrometheusProtobuf();
+                metricsBuilder.Configuration.Configure(options =>
+                {
+                    options.AddEnvTag();
+                    options.AddAppTag();
+                    options.AddServerTag();
+                    options.Enabled = true;
+                });
+                
+                metricsBuilder.ToInfluxDb(hostingContext.Configuration.GetSection("Metrics:Influxdb"));
+            });
+
+            return hostBuilder.UseMetrics(options =>
+            {
+                options.EndpointOptions = endpointsOptions =>
+                {
+                    endpointsOptions.EnvironmentInfoEndpointEnabled = true;
+                    endpointsOptions.MetricsTextEndpointEnabled = false;
+                    endpointsOptions.MetricsEndpointEnabled = true;
+                    endpointsOptions.MetricsEndpointOutputFormatter = Metrics.Instance.OutputMetricsFormatters.OfType<App.Metrics.Formatters.Prometheus.MetricsPrometheusTextOutputFormatter>().First();
+                };
+            });
+        }
+        #region Metrics
+
+        public static IMetricsBuilder ToInfluxDb(this IMetricsBuilder metricsBuilder, IConfigurationSection configurationSection)
+        {
+            #region report to influxdb
+            if (configurationSection.Exists())
+            {
+                var appMetrics_Influxdb_Enable = configurationSection["Enable"];
+                if (appMetrics_Influxdb_Enable.ToLower() == bool.TrueString.ToLower())
+                {
+                    metricsBuilder.Report.ToInfluxDb(options =>
+                    {
+                       
+                        var appMetrics_influxdb_address = configurationSection["Address"];
+                        var appMetrics_influxdb_database = configurationSection["Database"];
+                        var appMetrics_influxdb_username = configurationSection["UserName"];
+                        var appMetrics_influxdb_password = configurationSection["Password"];
+
+                        options.InfluxDb = new App.Metrics.Reporting.InfluxDB.InfluxDbOptions()
+                        {
+                            BaseUri = new Uri(appMetrics_influxdb_address),
+                            Database = appMetrics_influxdb_database,
+                            UserName = appMetrics_influxdb_username,
+                            Password = appMetrics_influxdb_password,
+                            CreateDataBaseIfNotExists = true,
+                        };
+                        options.HttpPolicy = new App.Metrics.Reporting.InfluxDB.Client.HttpPolicy
+                        {
+                            FailuresBeforeBackoff = int.Parse(configurationSection["Options:FailuresBeforeBackoff"] ?? "3"),
+                            BackoffPeriod = TimeSpan.FromSeconds(int.Parse(configurationSection["Options:BackoffPeriod"] ?? "30")),
+                            Timeout = TimeSpan.FromSeconds(int.Parse(configurationSection["Options:BackoffPeriod"] ?? "15"))
+                        };
+                        options.FlushInterval = TimeSpan.FromSeconds(int.Parse(configurationSection["Options:FlushInterval"] ?? "5"));
+
+                    });
+                }
+            }
+            #endregion
+
+            return metricsBuilder;
+        }
+        #endregion
+    }
+}
