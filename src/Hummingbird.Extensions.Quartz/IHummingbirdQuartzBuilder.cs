40923ee3a61d732585448a29b46eb799671026b4
GUOMING <geniusming@qq.com>
2021-10-13 18:43:14 +0800

    添添加Quartz

---
+++ src/Hummingbird.Extensions.Quartz/IHummingbirdQuartzBuilder.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hummingbird.Extensions.Tracing
+{
+    public interface IHummingbirdQuartzBuilder
+    {
+         IServiceCollection Services { get; }
+    }
+}
