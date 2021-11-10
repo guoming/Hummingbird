3c5035a96e6243e6545337157ae81707191a5940
guoming <geniusming@qq.com>
2021-11-10 09:14:04 +0800

    fix:confluent-kafka-dotnet 版本升级

---
+++ example/Hummingbird.WebApi/Jobs/HelloJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace Hummingbird.Example.Jobs
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Greetings from HelloJob!");
+        }
+    }
+}
