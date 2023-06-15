5316f8b4484c5ec90f5be507cbbb8b5b15cd295c
郭明 <zt7905@ztn.com>
2023-06-15 20:55:50 +0800

    fix: 修改消息Mysql持久化消息长度

--- src/Hummingbird.Extensions.EventBus.MySqlLogging/db.sql
+++ src/Hummingbird.Extensions.EventBus.MySqlLogging/db.sql
@@ -3,9 +3,10 @@ CREATE TABLE EventLogs
 (
 	EventId bigint not null  PRIMARY KEY,
 	MessageId varchar(50) not null,
-	Content NVARCHAR(max),
+	Content LONGTEXT NOT NULL,
 	CreationTime DATETIME2 NOT NULL,
 	EventTypeName  NVARCHAR(500),
 	State INT NOT NULL,
-	TimesSent INT NOT NULL
+	TimesSent INT NOT NULL,
+	TraceId varchar(50) not null,
 )
